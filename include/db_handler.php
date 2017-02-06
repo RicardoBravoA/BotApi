234a3148e5d4835609881e2959aa6b84dface233
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-06 12:43:25 -0500

    feat(ws): greeting added

--- include/db_handler.php
+++ include/db_handler.php
@@ -10,8 +10,173 @@ class DbHandler {
         $this->conn = $db->connect();
     }
 
+    /*
+    // All Brand
+    public function getAllBrand() {
+        $response = array();
+        $stmt = $this->conn->prepare("SELECT brand_id, description, image FROM brand");
+        if($stmt->execute()){
+            $stmt->bind_result($brand_id, $description, $image);
+            $stmt->store_result();
+            if($stmt->num_rows>0){
+                $data = array();
+                while ($stmt->fetch()) {
+                    $tmp = array();
+                    $tmp["brand_id"] = $brand_id;
+                    $tmp["description"] = $description;
+                    $tmp["image"] = $image;
+                    array_push($data, $tmp);
+                }
+                $_meta = array();
+                $_meta["status"]="success";
+                $_meta["code"]="200";
+                $response["_meta"] = $_meta;
+                $response["data"] = $data;
+                $stmt->close();
+                return $response;
+            }else{
+                $meta = array();
+                $meta["status"] = "error";
+                $meta["code"] = "101";
+                $response["_meta"] = $meta;
+            }
+        }else{
+            $meta = array();
+            $meta["status"] = "error";
+            $meta["code"] = "100";
+            $response["_meta"] = $meta;
+        }
+        return $response;
+    }
+    */
+
     // Message
     public function postMessage($message, $id){
+        $response = array();
+        $stmt = $this->conn->prepare("SELECT message_default_id, message, id, response_1, response_2, response_type_id FROM message_default WHERE message LIKE ? AND id = ?");
+        $stmt->bind_param("ss", $message, $id);
+        if($stmt->execute()){
+            $stmt->bind_result($message_default_id, $message, $id, $response_1, $response_2, $response_type_id);
+            $stmt->store_result();
+            if($stmt->num_rows>0){
+                $stmt->fetch();
+
+                if($response_type_id == 2){
+                    $response = array();
+                    $data1 = array();
+                    $data1["response_1"] = $response_1;
+                    $data1["response_2"] = $response_2;
+                    $data1["response_type_id"] = $response_type_id;
+
+                    $stmt2 = $this->conn->prepare("SELECT operation_type_id, operation_id, operation_description FROM operation_type");
+                    if($stmt2->execute()){
+                        $stmt2->bind_result($operation_type_id, $operation_id, $operation_description);
+                        $stmt2->store_result();
+                        if($stmt2->num_rows>0){
+                            $data2 = array();
+                            while ($stmt2->fetch()) {
+                                $tmp2 = array();
+                                $tmp2["operation_type_id"] = $operation_type_id;
+                                $tmp2["operation_id"] = $operation_id;
+                                $tmp2["operation_description"] = $operation_description;
+                                array_push($data2, $tmp2);
+                            }
+
+                            $_meta = array();
+                            $_meta["status"]="success";
+                            $_meta["code"]="200";
+                            $response["_meta"] = $_meta;
+                            $response["operation"] = $data2;
+                            $response["message"] = $data1;
+                            $stmt->close();
+                            return $response;
+                        }else{
+                            $meta = array();
+                            $meta["status"] = "error";
+                            $meta["code"] = "101";
+                            $response["_meta"] = $meta;
+                        }
+                    }else{
+                        $meta = array();
+                        $meta["status"] = "error";
+                        $meta["code"] = "100";
+                        $response["_meta"] = $meta;
+                    }
+
+
+                }
+
+
+
+
+                
+            }else{
+                $meta = array();
+                $meta["status"] = "error";
+                $meta["code"] = "101";
+                $response["_meta"] = $meta;
+            }
+        }else{
+            $meta = array();
+            $meta["status"] = "error";
+            $meta["code"] = "100";
+            $response["_meta"] = $meta;
+        }
+
+        return $response;
+    }
+
+    //Greetings
+    //public function greeting($response_1, $response_2, $response_type_id){
+    public function myFunction(){
+        $response = array();
+        //$data1 = array();
+        /*
+        $tmp1 = array();
+        $tmp1["response_1"] = $response_1;
+        $tmp1["response_2"] = $response_2;
+        $tmp1["response_type_id"] = $response_type_id;
+        array_push($data1, $tmp1);
+        */
+        $response["data"] = "hola";
+        return $response;
+        /*
+        $stmt = $this->conn->prepare("SELECT operation_type_id, operation_id, operation_description FROM operation_type");
+        if($stmt->execute()){
+            $stmt->bind_result($operation_type_id, $operation_id, $operation_description);
+            $stmt->store_result();
+            if($stmt->num_rows>0){
+                $data = array();
+                while ($stmt->fetch()) {
+                    $tmp = array();
+                    $tmp["operation_type_id"] = $operation_type_id;
+                    $tmp["operation_id"] = $operation_id;
+                    $tmp["operation_description"] = $operation_description;
+                    array_push($data, $tmp);
+                }
+
+                $_meta = array();
+                $_meta["status"]="success";
+                $_meta["code"]="200";
+                $response["_meta"] = $_meta;
+                $response["operation"] = $data;
+                $response["message"] = $data1;
+                $stmt->close();
+                return $response;
+            }else{
+                $meta = array();
+                $meta["status"] = "error";
+                $meta["code"] = "101";
+                $response["_meta"] = $meta;
+            }
+        }else{
+            $meta = array();
+            $meta["status"] = "error";
+            $meta["code"] = "100";
+            $response["_meta"] = $meta;
+        }
+        */
+        //return $response;
 
     }
 

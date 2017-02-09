cf633f6a7b03128d9bb5d2b806c9f6745d5e0c74
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-09 11:17:32 -0500

    feat(message): default message

--- include/db_handler.php
+++ include/db_handler.php
@@ -54,8 +54,8 @@ class DbHandler {
     public function postMessage($message, $operation, $property_type){
         $response = array();
         $error = true;
-        $stmt = $this->conn->prepare("SELECT message_default_id, message, operation, property_type, response_1, response_2, response_type_id FROM message_default WHERE message LIKE ? AND operation = ? AND property_type = ?");
-        $stmt->bind_param("sss", $message, $operation, $property_type);
+        $stmt = $this->conn->prepare("SELECT message_default_id, message, operation, property_type, response_1, response_2, response_type_id FROM message_default WHERE message LIKE '%$message%' AND operation = ? AND property_type = ?");
+        $stmt->bind_param("ss", $operation, $property_type);
         if($stmt->execute()){
             $stmt->bind_result($message_default_id, $message, $operation, $property_type, $response_1, $response_2, $response_type_id);
             $stmt->store_result();
@@ -63,7 +63,6 @@ class DbHandler {
                 $stmt->fetch();
 
                 if($response_type_id == 2){
-                    $response = array();
                     $data1 = array();
                     $data1["response_1"] = $response_1;
                     $data1["response_2"] = $response_2;
@@ -105,7 +104,6 @@ class DbHandler {
 
 
                 } else if($response_type_id == 3){
-                    $response = array();
                     $data1 = array();
                     $data1["response_1"] = $response_1;
                     $data1["response_2"] = $response_2;
@@ -145,7 +143,6 @@ class DbHandler {
                         $response["_meta"] = $meta;
                     }
                 } else if($response_type_id == 4){
-                    $response = array();
                     $data1 = array();
                     $data1["response_1"] = $response_1;
                     $data1["response_2"] = $response_2;
@@ -195,22 +192,59 @@ class DbHandler {
                 }
                 
             }else{
-                $meta = array();
-                $meta["status"] = "error";
-                $meta["code"] = "101";
-                $response["_meta"] = $meta;
+                $data1 = array();
+                $data1["response_1"] = "Hola";
+                $data1["response_2"] = "¿Qué operación desea realizar?";
+                $data1["response_type_id"] = "2";
+
+                $stmt2 = $this->conn->prepare("SELECT DISTINCT operation FROM message_default WHERE operation != '' ORDER BY operation");
+                    if($stmt2->execute()){
+                        $stmt2->bind_result($operation);
+                        $stmt2->store_result();
+                        if($stmt2->num_rows>0){
+                            $data2 = array();
+                            while ($stmt2->fetch()) {
+                                $tmp2 = array();
+                                $tmp2["operation"] = $operation;
+                                array_push($data2, $tmp2);
+                            }
+
+                            $_meta = array();
+                            $_meta["status"]="success";
+                            $_meta["code"]="200";
+                            $response["_meta"] = $_meta;
+                            $response["type"] = "operation";
+                            $response["operation"] = $data2;
+                            $response["message"] = $data1;
+                            $stmt->close();
+                            $error = false;
+                        }else{
+                            $meta = array();
+                            $meta["status"] = "error";
+                            $meta["code"] = "101";
+                            $response["_meta"] = $meta;
+                        }
+                }else{
+                    $meta = array();
+                    $meta["status"] = "error";
+                    $meta["code"] = "100";
+                    $response["_meta"] = $meta;
+                }
             }
-        }else{
-            $meta = array();
-            $meta["status"] = "error";
-            $meta["code"] = "100";
-            $response["_meta"] = $meta;
         }
 
         echoResponse($error, $response);
     }
 
-    
+    function echoResponse($isError, $response) {
+        $app = \Slim\Slim::getInstance();
+        $app->status(200);
+        if($isError){
+            $app->status(500);
+        }
+        $app->contentType('application/json');
+        echo json_encode($response);
+    }
 
 
 }
